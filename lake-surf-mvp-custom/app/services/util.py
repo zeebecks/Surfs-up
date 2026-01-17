c35f37fa684f4aa349ee28fd9f092361ba2db30a
zeebecks <zacharybecka@gmail.com>
2026-01-17 10:00:00 -0600

    removed inner cloned directory

--- lake-surf-mvp-custom/app/services/util.py
+++
@@ -1,9 +0,0 @@
-from contextlib import contextmanager
-from ..database import SessionLocal
-@contextmanager
-def get_session():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
